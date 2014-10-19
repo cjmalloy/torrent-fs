4645216a513802438b3953afbc421b99b8be8bba
Chris Malloy <cjmalloy@gmail.com>
2014-10-19 15:02:00 -0300

    Just forward the index page.

--- tfs-server/src/main/java/com/cjmalloy/torrentfs/server/remote/rest/HtmlExtensionApi.java
+++ tfs-server/src/main/java/com/cjmalloy/torrentfs/server/remote/rest/HtmlExtensionApi.java
@@ -1,11 +1,13 @@
 package com.cjmalloy.torrentfs.server.remote.rest;
 
+import java.net.URI;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
 import com.cjmalloy.torrentfs.extension.HtmlExtension;
 import com.cjmalloy.torrentfs.server.TfsClientSingleton;
@@ -14,12 +16,12 @@ import com.cjmalloy.torrentfs.server.TfsClientSingleton;
 public class HtmlExtensionApi
 {
     @GET @Path("/index/{info_hash}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String torrentStatus(@PathParam("info_hash") String infoHash)
+    public Response getIndex(@PathParam("info_hash") String infoHash)
     {
         HtmlExtension ext = (HtmlExtension) TfsClientSingleton.get().getTorrent(infoHash).tfsData.extensions.get("html");
         if (ext == null) throw new WebApplicationException(400);
 
-        return ext.index;
+        URI uri = UriBuilder.fromUri("/ext/html/static/" + infoHash + "/" + ext.index).build();
+        return Response.seeOther(uri).build();
     }
 }
